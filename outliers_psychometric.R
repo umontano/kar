82bae3b1a45f714d1fcc9e5a81b1135fa70e9206
umontano <u.montano.j@gmail.com>
2022-10-08 22:10:45 +0000

    vi edit

--- outliers_psychometric.R
+++ outliers_psychometric.R
@@ -8,6 +8,8 @@ print(ntimes)
 }
 
 
+#==========================================
+#==========================================
 items <- read.csv('imputed_items_valkarmfs.csv', header=TRUE)
  # scales <- read.csv('imputed_scales_valkarmfs.csv', header=TRUE)[, -1]
  # factors <- read.csv('imputed_broad_dimensions_valkarmfs.csv', header=TRUE)[, -1]
@@ -20,34 +22,36 @@ multiappended_dataset <- items
 set.seed(1111)
 #lapply(1:9, reappend_dataset_multiple_times, appendee_dataset=items)
 
-
 date_time <- format(Sys.time(), 'x%y%m%d_%Hh%Mm%Ss_')
 
 
+#==========================================
+    #Function to clean-up outlaiers
+#==========================================
+    place_na_in_otlaiers <- function(column_outlaieree) {
+        for(iteration_column in 1:10) {
+            outlaiers <- boxplot.stats(column_outlaieree)$out
+            column_outlaieree[which(column_outlaieree %in% outlaiers)] <- NA
+            #Creates a boolean value checking if there is still outlaiers after being removed, so tgat it can be evaluated
+            #boolean_evaluation <- length(boxplot.stats(column_outlaieree)$out) > 0
+                if(length(boxplot.stats(column_outlaieree)$out) > 1) {
+                    print(paste0('========', iteration_column))
+                    print(which(column_outlaieree %in% outlaiers))
+                }
+        if(! length(boxplot.stats(column_outlaieree)$out) > 1) break
+        }
+    }
+#==========================================
+#==========================================
 
+
+
+
+#==========================================
+#==========================================
 #Loop identify outlaiers and place NA 
 for(iteration_dataset in 1:20) {
-	i <- 1
-	for(column_outlaieree in items) {
-        for(iteration_column in 1:20) {
-            outlaiers <- boxplot.stats(column_outlaieree)$out
-            column_outlaieree[which(column_outlaieree %in% outlaiers))] <- NA
-        fill_outlaiers <- function(oooo) return(oooo)
-        column_outlaieree <- fill_outlaiers(column_outlaieree)
-
-
-        #Creates a boolean value checking if there is still outlaiers after being removed, so tgat it can be evaluated
-        boolean_evaluation <- length(boxplot.stats(column_outlaieree)$out) > 0
-        i <- i+1
-            if(length(boxplot.stats(column_outlaieree)$out) > 1) {
-                print(i)
-                print('========')
-                print(boxplot.stats(column_outlaieree)$out)
-                print(which(column_outlaieree %in% outlaiers))
-                print(which(column_outlaieree %in% outlaiers))
-            }
-        if(! length(boxplot.stats(column_outlaieree)$out) > 1) break
-    }
+    items[] <- lapply(items, place_na_in_otlaiers)
 }
 
 summary(column_outlaieree)['3rd Qu.']
@@ -60,6 +64,7 @@ sliceditems <- items[,74:76]
 i <- 1
 for(outlieree_column in sliceditems) {
 	if(length(boxplot.stats(outlieree_column)$out) > 1) {
+		print('========')
 		print(i)
 		print('========')
 		print(boxplot.stats(outlieree_column)$out)
